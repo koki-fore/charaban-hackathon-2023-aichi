a40c6cc9633814df1caba2c4a215eb26499f86fe
kuu13580 <13580kuu@gmail.com>
2023-11-11 16:06:57 +0900

    init PostCardコンポーネント

---
+++ front/src/components/PostCard.jsx
@@ -0,0 +1,25 @@
+import "../styles/PostCard.css";
+
+export const PostCard = ({ /** @type {Post} */ post }) => {
+  return (
+    <></>
+  );
+};
+
+const CardHeader = () => {
+  return (
+    <></>
+  );
+}
+
+const CardContent = () => {
+  return (
+    <></>
+  );
+}
+
+const CardFooter = () => {
+  return (
+    <></>
+  );
+}
