e077bd221b6619b381f3a82b9ad0a098db49459f
koki-fore <furukunn@gmail.com>
2023-11-11 15:55:08 +0900

    prettierを適用

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,79 +1,82 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
 import logo from '../assets/logo.png'
-import { Button } from '@mui/material';
+import { Button } from '@mui/material'
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
 
 const Header = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const [isLogin, setIsLogin] = React.useState(false);
+  const [anchorElUser, setAnchorElUser] = React.useState(null)
+  const [isLogin, setIsLogin] = React.useState(false)
 
   const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElUser(event.currentTarget)
+  }
 
   const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    setAnchorElUser(null)
+  }
 
   return (
-    <AppBar position="static" sx={{backgroundColor: '#84E9FF'}}>
+    <AppBar position="static" sx={{ backgroundColor: '#84E9FF' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <img src={logo} alt="logo" />
 
-          
           <Box sx={{ flexGrow: 1, display: 'flex' }}></Box>
-          {isLogin ?
-          <Box sx={{ flexGrow: 0 }}>  
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> 
-          : 
-          <Box sx={{ flexGrow: 0, display: 'flex' }}>
-            <Button variant="contained" sx={{borderRadius: 5, mr:1, width:100}}>新規登録</Button>
-            <Button variant="text" sx={{borderRadius: 5, ml:1, backgroundColor:"white", width:100}}>ログイン</Button>
-          </Box>
-          }
-          
+          {isLogin ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}>
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <Box sx={{ flexGrow: 0, display: 'flex' }}>
+              <Button variant="contained" sx={{ borderRadius: 5, mr: 1, width: 100 }}>
+                新規登録
+              </Button>
+              <Button
+                variant="text"
+                sx={{ borderRadius: 5, ml: 1, backgroundColor: 'white', width: 100 }}>
+                ログイン
+              </Button>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
-  );
+  )
 }
 export default Header
