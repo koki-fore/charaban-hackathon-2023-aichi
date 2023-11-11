f6eb4753a146783b7e39f51592d34a8c9db10968
koki-fore <furukunn@gmail.com>
2023-11-11 15:13:22 +0900

    ヘッダー作成 Fixes #4

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline'
 // import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CssBaseline />
+      <Header />
     </BrowserRouter>
   </React.StrictMode>,
 )
