be1f0a2b7d5068f70f755d33eb4b735f7173f8a2
kuu13580 <13580kuu@gmail.com>
2023-11-11 17:48:30 +0900

    add デフォルトテーマ

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,14 +4,31 @@ import CssBaseline from '@mui/material/CssBaseline'
 // import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header.jsx'
-import { PostCard } from './components/PostCard.jsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { PostCard } from './components/'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Noto Sans JP', 'sans-serif'].join(','),
+  },
+  palette: {
+    text: {
+      primary: '#333',
+    },
+    grayText: '#979797',
+    bgGray: '#F8F9FA',
+    bgSky: '#84E9FF',
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssBaseline />
-      <Header />
-      <PostCard />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <PostCard />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
