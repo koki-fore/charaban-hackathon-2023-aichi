189e88d5c053158e609a42d366eed1f52cf705cf
kuu13580 <13580kuu@gmail.com>
2023-11-11 19:22:29 +0900

    add PostCardFooter

--- front/src/components/PostCard.jsx
+++ front/src/components/PostCard.jsx
@@ -1,6 +1,7 @@
 import '../styles/PostCard.css'
 import { PostCardHeader, PostCardContent, PostCardFooter } from './card'
 import { Box } from '@mui/material'
+import { useState } from 'react'
 
 /**
  * @typedef {Object} User
@@ -10,13 +11,23 @@ import { Box } from '@mui/material'
  */
 
 /**
- * @typedef {Object} Post
+ * @typedef {Object} PostResponse
  * @property {number} id
  * @property {number} user_FK
  * @property {string} before_picture_path
  * @property {string} after_picture_path
  * @property {string} after_text
  * @property {string} before_text
+ * @property {string} created_at
+ * @property {User} user
+ */
+
+/**
+ * @typedef {Object} Post
+ * @property {number} id
+ * @property {number} user_FK
+ * @property {string} picture_path
+ * @property {string} text
  * @property {string} created_at
  * @property {User} user
  */
@@ -37,14 +48,87 @@ const duymmyPost = {
   },
 }
 
-export const PostCard = ({ /** @type {Post} */ post }) => {
+export const PostCard = ({ /** @type {PostResponse} */ post }) => {
+  const [postRotate, setPostRotate] = useState(false)
+  const [isLiked, setIsLiked] = useState(false)
+  const [numLiked, setNumLiked] = useState(0)
   post = duymmyPost
+
+  const toggleRotate = () => {
+    setPostRotate(!postRotate)
+  }
+
+  // 前後の投稿を分離する
+  /** @type {Post} */
+  const beforePost = {
+    ...post,
+    picture_path: post.before_picture_path,
+    text: post.before_text,
+  }
+  /** @type {Post} */
+  const afterPost = {
+    ...post,
+    picture_path: post.after_picture_path,
+    text: post.after_text,
+  }
+
+  const handleLike = () => {
+    console.log('like')
+    // TODO: APIを投げてresponseをセットする
+    if (isLiked) {
+      setIsLiked(false)
+    } else {
+      setIsLiked(true)
+      setNumLiked((prev) => prev + 1)
+    }
+  }
+
   return (
     <>
-      <Box className="PostCard" sx={{ maxWidth: 600, mx: 'auto' }} data-post-id={post.id}>
+      <Box
+        className={`PostCard-wrapper ${postRotate && 'PostCard-wrapper--rotate'}`}
+        sx={{ maxWidth: 600, mx: 'auto', p: 2 }}
+        data-post-id={post.id}>
+        <PostCardChild
+          post={beforePost}
+          className={'PostCard PostCard-front'}
+          toggleRotate={toggleRotate}
+          handleLike={handleLike}
+          isLiked={isLiked}
+          numLiked={numLiked}
+        />
+        <PostCardChild
+          post={afterPost}
+          className={'PostCard PostCard-back'}
+          toggleRotate={toggleRotate}
+          handleLike={handleLike}
+          isLiked={isLiked}
+          numLiked={numLiked}
+        />
+      </Box>
+    </>
+  )
+}
+
+const PostCardChild = ({
+  /** @type {Post} */ post,
+  className,
+  toggleRotate,
+  handleLike,
+  isLiked,
+  numLiked,
+}) => {
+  return (
+    <>
+      <Box className={className} sx={{ maxWidth: 600, mx: 'auto', p: 2 }} data-post-id={post.id}>
         <PostCardHeader user={post.user} datetime={post.created_at} />
         <PostCardContent />
-        <PostCardFooter />
+        <PostCardFooter
+          toggleRotate={toggleRotate}
+          handleLike={handleLike}
+          isLiked={isLiked}
+          numLiked={numLiked}
+        />
       </Box>
     </>
   )
--- front/src/components/card/CardFooter.jsx
+++ front/src/components/card/CardFooter.jsx
@@ -1,3 +1,63 @@
-export const PostCardFooter = () => {
-  return <></>
+import { Box, IconButton } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+
+export const PostCardFooter = ({ toggleRotate, handleLike, isLiked, numLiked, sx }) => {
+  const handleComment = () => {
+    console.log('comment')
+  }
+
+  return (
+    <>
+      <Box sx={sx}>
+        <Grid container justifyContent="space-around">
+          <Grid xs="auto">
+            <IconButton sx={{ p: 0 }}>
+              <Box sx={{ width: 30, height: 30 }}>
+                <img
+                  src="/comment.svg"
+                  alt="to comment"
+                  onClick={handleComment}
+                  width={30}
+                  height={30}
+                />
+              </Box>
+            </IconButton>
+          </Grid>
+          <Grid xs="auto">
+            <IconButton sx={{ p: 0 }}>
+              <Box
+                sx={{
+                  width: 30,
+                  height: 30,
+                  '&::after': {
+                    content: `'${numLiked || ''}'`,
+                    position: 'absolute',
+                    top: '50%',
+                    left: '110%',
+                    transform: 'translate(0, -50%)',
+                    color: 'grayText',
+                    fontSize: 14,
+                  },
+                }}>
+                <img
+                  src={isLiked ? '/stared.svg' : '/star-outline.svg'}
+                  alt="like"
+                  onClick={() => handleLike()}
+                  width={30}
+                  height={30}
+                />
+              </Box>
+            </IconButton>
+          </Grid>
+          <Grid xs="auto">
+            <IconButton sx={{ p: 0 }}>
+              <Box sx={{ width: 30, height: 30 }}>
+                <img src="/Z.png" alt="rotete" onClick={() => toggleRotate()} height={30} />
+              </Box>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  )
 }
