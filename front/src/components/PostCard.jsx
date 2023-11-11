ea8097ab2702ad42e953c0f58f55b9bb21235384
kuu13580 <13580kuu@gmail.com>
2023-11-11 17:01:57 +0900

    add 投稿データの型定義とダミー

--- front/src/components/PostCard.jsx
+++ front/src/components/PostCard.jsx
@@ -1,8 +1,42 @@
 import '../styles/PostCard.css'
 import { PostCardHeader, PostCardContent, PostCardFooter } from './card'
-import { Box } from '@material-ui/core'
+import { Box } from '@mui/material'
+
+/**
+ * @typedef {Object} User
+ * @property {number} id
+ * @property {string} screen_name
+ * @property {string} profile_picture_path
+ */
+
+/**
+ * @typedef {Object} Post
+ * @property {number} id
+ * @property {number} user_FK
+ * @property {string} before_picture_path
+ * @property {string} after_picture_path
+ * @property {string} after_text
+ * @property {string} before_text
+ * @property {User} user
+ */
+
+/** @type { Post } */
+const duymmyPost = {
+  id: 1,
+  user_FK: 1,
+  before_picture_path: 'https://picsum.photos/200/300',
+  after_picture_path: 'https://picsum.photos/200/300',
+  after_text: 'after_text',
+  before_text: 'before_text',
+  user: {
+    id: 1,
+    screen_name: 'screen_name',
+    profile_picture_path: 'https://picsum.photos/200/300',
+  },
+}
 
 export const PostCard = ({ /** @type {Post} */ post, sx }) => {
+  post = duymmyPost
   return (
     <>
       <Box className="PostCard" sx={sx} data-post-id={post.id}>
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,12 +4,14 @@ import CssBaseline from '@mui/material/CssBaseline'
 // import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header.jsx'
+import { PostCard } from './components/PostCard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <CssBaseline />
       <Header />
+      <PostCard />
     </BrowserRouter>
   </React.StrictMode>,
 )
