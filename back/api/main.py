d277a8ce0d25bf9828726c69ec1450f02c5ca0a3
Kohtaroh Sakaue <spikekohtaroh@gmail.com>
2023-11-14 21:32:47 +0900

    first commit for routers of posts

--- back/api/main.py
+++ back/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import *
+from routers import posts
 
 app = FastAPI()
+app.include_router(posts.router)
 
 # CORS設定
 app.add_middleware(
