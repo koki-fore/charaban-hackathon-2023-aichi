71b40a26d92a26de53d10e0528aa5a831b121ecf
Kohtaroh Sakaue <spikekohtaroh@gmail.com>
2023-11-16 16:03:37 +0900

    refactor posts

--- back/api/cruds/posts.py
+++ back/api/cruds/posts.py
@@ -1,22 +1,24 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.engine import Result
-from sqlalchemy import select
-from datetime import datetime
+# from sqlalchemy.engine import Result
+# from sqlalchemy import select
+# from datetime import datetime
 from typing import List
 
-from models.db_models import Post as Post_model, Comment as Comment_model
-from schemas.post_schema import PostCreate as PostCreate_schema
-from schemas.post_schema import PostUpdate as PostUpdate_schema
-from schemas.post_schema import Post as Post_schema
-from schemas.post_schema import PostWithUser as PostWithUser_schema
-from schemas.post_schema import PostWithComment as PostWithComment_schema
+import models.db_models as db_models
+import schemas.post_schema as posts_schema
+#from models.db_models import Post as Post_model, Comment as Comment_model
+#from schemas.post_schema import PostCreate as PostCreate_schema
+#from schemas.post_schema import PostUpdate as PostUpdate_schema
+#from schemas.post_schema import Post as Post_schema
+#from schemas.post_schema import PostWithUser as PostWithUser_schema
+#from schemas.post_schema import PostWithComment as PostWithComment_schema
 
 
 def create_post(
     db: Session,
-    post_create: PostCreate_schema
-    ) -> Post_model:
-    post = Post_model(**post_create.model_dump())
+    post_create: posts_schema.PostCreate
+    ) -> db_models.Post:
+    post = db_models.Post(**post_create.model_dump())
     db.add(post)
     db.commit()
     db.refresh(post)
@@ -24,28 +26,28 @@ def create_post(
 
 def get_all_posts(
     db: Session
-    ) -> List[Post_schema]:
-    return db.query(Post_model).all()
+    ) -> List[posts_schema.Post]:
+    return db.query(db_models.Post).all()
 
 def get_post(
     db: Session,
     post_id: int
-    ) -> Post_schema:
-    return db.query(Post_model).filter(Post_model.id == post_id).first()
+    ) -> posts_schema.Post:
+    return db.query(db_models.Post).filter(db_models.Post.id == post_id).first()
 
 def get_post_with_comments(
     db: Session,
     post_id: int
-    ) -> PostWithComment_schema:
-    return db.query(Post_model).filter(Post_model.id == post_id).first()
+    ) -> posts_schema.PostWithComment:
+    return db.query(db_models.Post).filter(db_models.Post.id == post_id).first()
 
 def get_recommended_posts():
     pass
 
 def update_post_is_delete(
     db: Session,
-    original: Post_schema
-    ) -> Post_model:
+    original: posts_schema.Post
+    ) -> db_models.Post:
     original.is_deleted = True
     db.add(original)
     db.commit()
@@ -55,8 +57,8 @@ def update_post_is_delete(
 def delete_post(
     db: Session,
     post_id: int
-    ) -> Post_model:
-    post = db.query(Post_model).filter(Post_model.id == post_id).first()
+    ) -> db_models.Post:
+    post = db.query(db_models.Post).filter(db_models.Post.id == post_id).first()
     db.delete(post)
     db.commit()
     return
