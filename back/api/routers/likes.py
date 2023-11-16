d69626a0c476fb62686f71b2b948295ef622328f
Kohtaroh Sakaue <spikekohtaroh@gmail.com>
2023-11-16 14:10:38 +0900

    likes作成

---
+++ back/api/routers/likes.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+
+import cruds.likes as likes_cruds
+import schemas.like_schema as likes_schema
+from db_sync_session import db_session
+
+router = APIRouter()
+
+@router.post("/likes", response_model=likes_schema.Like)
+def create_like(
+    like_body: likes_schema.LikeCreate,
+    db: Session = Depends(db_session)
+    ) -> likes_schema.Like:
+    return likes_cruds.create_like(db, like_body)
+
+@router.get("/likes", response_model=List[likes_schema.Like])
+def list_likes(
+    db: Session = Depends(db_session)
+    ) -> List[likes_schema.Like]:
+    return likes_cruds.get_all_likes(db)
+
+@router.delete("/likes/{like_id}")
+def delete_like(
+    like_id: int,
+    db: Session = Depends(db_session)
+    ) -> likes_schema.Like:
+    return likes_cruds.delete_like(db, like_id)
+
+@router.get("/likes-count/{post_id}")
+def get_likes_count(
+    post_id: int,
+    db: Session = Depends(db_session)
+    ) -> int:
+    return likes_cruds.get_likes_count(db, post_id)
