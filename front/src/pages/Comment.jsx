e717037f05d9429d0412b7a3bcd7948d60f32748
kuu13580 <13580kuu@gmail.com>
2023-11-18 04:03:31 +0900

    fix 型定義

--- front/src/pages/Comment.jsx
+++ front/src/pages/Comment.jsx
@@ -12,6 +12,15 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack'
  */
 
 /**
+ * @typedef {Object} Comment
+ * @property {number} id
+ * @property {number} user_FK
+ * @property {string} text
+ * @property {string} created_at
+ * @property {User} users
+ */
+
+/**
  * @typedef {Object} PostResponse
  * @property {number} id
  * @property {number} user_FK
@@ -21,6 +30,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack'
  * @property {string} before_text
  * @property {string} created_at
  * @property {User} users
+ * @property {Comment[]} comments
  */
 
 /**
@@ -32,23 +42,6 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack'
  * @property {string} created_at
  * @property {User} users
  */
-
-/** @type { PostResponse } */
-const duymmyPost = {
-  id: 1,
-  user_FK: 1,
-  before_picture_path: 'https://picsum.photos/200/300',
-  after_picture_path: 'https://picsum.photos/400/300',
-  after_text:
-    '6イニング3自責点は ERA (Earned Run Average＝防御率) にすると 4.5 となり、あまり良いとは言えないように見えるが、2010年シーズン以降の MLB 全体の防御率が 4.08 (2010)、3.94 (2011)、4.01 (2012) であることを見れば、現在でも妥当なラインだと思われる。',
-  before_text: 'before_text',
-  created_at: '2021-10-01 00:00:00',
-  user: {
-    id: 1,
-    screen_name: 'ユーザー名',
-    profile_picture_path: 'https://picsum.photos/200/300',
-  },
-}
 
 /** @type { Comment[] } */
 const dummyComments = [
@@ -90,9 +83,27 @@ const dummyComments = [
   },
 ]
 
+/** @type { PostResponse } */
+const duymmyPost = {
+  id: 1,
+  user_FK: 1,
+  before_picture_path: 'https://picsum.photos/200/300',
+  after_picture_path: 'https://picsum.photos/400/300',
+  after_text:
+    '6イニング3自責点は ERA (Earned Run Average＝防御率) にすると 4.5 となり、あまり良いとは言えないように見えるが、2010年シーズン以降の MLB 全体の防御率が 4.08 (2010)、3.94 (2011)、4.01 (2012) であることを見れば、現在でも妥当なラインだと思われる。',
+  before_text: 'before_text',
+  created_at: '2021-10-01 00:00:00',
+  users: {
+    id: 1,
+    screen_name: 'ユーザー名',
+    profile_picture_path: 'https://picsum.photos/200/300',
+  },
+  comments: dummyComments,
+}
+
 const Comment = () => {
   const { postId } = useParams()
-  const [post, setPost] = useState(null)
+  const [post, setPost] = useState({})
   const navigate = useNavigate()
 
   useEffect(() => {
