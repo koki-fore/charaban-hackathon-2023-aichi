c51c80791e8b5daf5aa36a46f098d304c79851c3
kuu13580 <13580kuu@gmail.com>
2023-11-18 13:53:17 +0900

    add プロフィール画面

--- front/src/pages/UserProfile.jsx
+++ front/src/pages/UserProfile.jsx
@@ -1,53 +1,67 @@
 import { useState, useEffect } from 'react'
-import { Box, Container, Button, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { Box, Container, Button, Typography, Grid } from '@mui/material'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useAuthContext } from '../context/AuthContext'
+import { api } from '../util/axios'
 
 const UserProfile = () => {
-    const navigate = useNavigate()
-    const { authApi } = useAuthContext()
-    const [userInfo, setUserInfo] = useState()
+  const navigate = useNavigate()
+  const { userId } = useParams()
+  const { user } = useAuthContext()
+  const [userInfo, setUserInfo] = useState()
 
-    useEffect(() => {
-        const fetchUser = () => {
-            authApi.get("/users/me").
-            then( (user) => {
-                setUserInfo(user)
-            }).catch((error) => {
-                console.log(error)
-            })
-        }
-        fetchUser()
-    }, [])
+  const fetchUser = () => {
+    api
+      .get(`/users/${userId}`)
+      .then((res) => {
+        const user = res.data
+        setUserInfo(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    return (
-        <Container component="main" maxWidth="sm" sx={{ marginTop: 5 }} >
-            <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Box
-                    component='img'
-                    // src={userInfo.profile_picture_path}
-                    src=''
-                    alt="プロフィール画像"
-                    sx={{width: '50%'}}
-                />
-                <Box>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{width: '50%', borderRadius: 5}}
-                        onClick={()=>{navigate("/updateAccount")}}
-                    >
-                    編集
-                    </Button>
-                </Box>
-            </Box>
-            <Typography variant='body1'>ユーザー名：</Typography>
-            <Typography variant='h6'>{userInfo.screen_name}</Typography>
-            <Typography variant='body1'>自己紹介：</Typography>
-            <Typography variant='h6'>{userInfo.description}</Typography>
-        </Container>
+  useEffect(() => {
+    if (!user) return
+    fetchUser()
+  }, [user])
+
+  return (
+    userInfo && (
+      <Container component="main" maxWidth="sm" sx={{ marginTop: 5 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+          <Box
+            component="img"
+            // src={userInfo.profile_picture_path}
+            src=""
+            alt="プロフィール画像"
+            sx={{ width: '50%' }}
+          />
+          <Box>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ width: '50%', borderRadius: 5 }}
+              onClick={() => {
+                navigate('/updateAccount')
+              }}>
+              編集
+            </Button>
+          </Box>
+        </Box>
+        <Grid container alignItems="center">
+          <Typography variant="body1">ユーザー名：</Typography>
+          <Typography variant="h6">{userInfo.screen_name}</Typography>
+        </Grid>
+        <Grid container alignItems="center">
+          <Typography variant="body1">自己紹介：</Typography>
+          <Typography variant="h6">{userInfo.description}</Typography>
+        </Grid>
+      </Container>
     )
+  )
 }
 
-export default UserProfile;
+export default UserProfile
